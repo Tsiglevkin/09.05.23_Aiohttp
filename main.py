8bc4e45565bba1e0e42e2b98481e01e6f0f99945
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2023-05-15 17:15:02 +0500

    change main and views.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 from function import orm_context, session_middleware
+from views import UserView, AdvertisementView
 
 app = web.Application()  # создаем экземпляр aiohttp приложения.
 
@@ -9,6 +10,16 @@ app.cleanup_ctx.append(orm_context)  # регистрируем контекст
 # Для открытия и закрытия сессии при выполнении методов get, post и т.д. используем созданную функцию session_middleware
 # добавим ее в список middleware нашего приложения.
 app.middlewares.append(session_middleware)
+
+# прописываем роуты
+app.add_routes(
+    [
+        web.post('/users/', UserView),
+        web.get('/users/{user_id:\d+}', UserView),
+        web.patch('/users/{user_id:\d+}', UserView),
+        web.delete('/users/{user_id:\d+}', UserView),
+    ]
+)
 
 # запускаем приложение.
 web.run_app(app)
--- views.py
+++ views.py
@@ -1,4 +1,8 @@
+import json
+
 from aiohttp import web
+from sqlalchemy.exc import IntegrityError
+
 from models import User, Advertisement
 from function import get_orm_item, hash_password
 from schema import validate
@@ -30,12 +34,16 @@ class UserView(web.View):
 
         user_data = await self.request.json()  # принятые данные из request
         validated_data = validate(json_data=user_data, model_class=User)  # валидируем данные при помощи написанной ф-и.
-        user_password = validated_data.get('password')  # достаем пароль
+        user_password = validated_data.get('user_pass')  # достаем пароль
         hashed_pass = hash_password(user_password)  # хэшируем его.
-        validated_data['password'] = hashed_pass  # вставляем обратно в валидированные данные.
+        validated_data['user_pass'] = hashed_pass  # вставляем обратно в валидированные данные.
         new_user = User(**validated_data)  # создаем нового пользователя
         self.request.get('session').add(new_user)  # Добавляем в сессии. Сессия находится в request['session']
-        await self.request.get('session').commit()  # делаем коммит.
+        try:
+            await self.request.get('session').commit()  # делаем коммит.
+        except IntegrityError as error:  # если пользователь существует, генерим ошибку.
+            raise web.HTTPConflict(text=json.dumps({'status': 'user already exists'}), content_type='application/json')
+
         return web.json_response({'user_id': new_user.id})  # возвращаем json с данными пользователя.
 
     async def patch(self):
@@ -47,14 +55,14 @@ class UserView(web.View):
         """
 
         user_id = int(self.request.match_info.get('user_id'))  # достаем id пользователя.
-        json_data = await self.request.json()  # получаем данные из запроса
-        validated_data = validate(json_data=json_data, model_class=User)  # валидируем данные
-        if 'password' in validated_data:  # если есть пароль в данных, хэшируем его
-            validated_data['password'] = hash_password(validated_data.get('password'))
+        user_data = await self.request.json()  # получаем данные из запроса
+        user_validated_data = validate(json_data=user_data, model_class=User)  # валидируем данные
+        if 'user_pass' in user_validated_data:  # если есть пароль в данных, хэшируем его
+            user_validated_data['user_pass'] = hash_password(user_validated_data.get('user_pass'))
 
         # получаем пользователя из БД
         user = await get_orm_item(item_class=User, item_id=user_id, session=self.request['session'])
-        for field, value in validated_data.items():  # устанавливаем валидные значения пользователю.
+        for field, value in user_validated_data.items():  # устанавливаем валидные значения пользователю.
             setattr(user, field, value)
         self.request['session'].add(user)  # добавляем данные в БД
         await self.request['session'].commit()  # делаем коммит
